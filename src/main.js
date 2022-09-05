3d89378af445b9a83c4ca7f91d3b84a726c986e4
Osman Yüksel <35294947+osman-yuksel@users.noreply.github.com>
2022-09-05 13:07:27 +0300

    Base implementation completed.

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ addButton.addEventListener("click", () => { AddToDo(addContent.value); })
 
 async function GetToDos(){
     await fetch(url).then(res => res.json())
-    .then(todos => {todos.map((todo) => { 
+    .then((todos) => { todoContainer.innerHTML = ''; todos.map((todo) => { 
+
+        addContent.value = "";
         
         const element = document.createElement("div");
         element.classList.add("todo");
@@ -16,19 +18,26 @@ async function GetToDos(){
         const completedCheckbox = document.createElement("input");
         completedCheckbox.type = "checkbox";
         completedCheckbox.checked = todo.isCompleted;
-        completedCheckbox.addEventListener("change", (element) => { UpdateToDo(element.target.parentNode) })
+        completedCheckbox.addEventListener("change", (element) => { UpdateToDo(element.target.parentNode, "") })
         element.appendChild(completedCheckbox);
 
         
         const todoContent = document.createElement("p");
-        todoContent.innerHTML = `${todo.id} ${todo.content}`;
+        todoContent.innerHTML = todo.content;
         element.appendChild(todoContent);
+        
+        const editButton = document.createElement("button");
+        editButton.innerHTML = "Edit";
+        editButton.type = "button";
+        editButton.addEventListener("click", (element) => { EditMode(element.target.parentNode) });
+        element.appendChild(editButton);
         
         const deleteButton = document.createElement("button");
         deleteButton.innerHTML = 'X';
         deleteButton.type = "button";
         deleteButton.addEventListener("click", (element) => { DeleteToDo(element.target.parentNode.dataset.id) });
         element.appendChild(deleteButton);
+
 
         todoContainer.insertBefore(element, todoContainer.firstChild) })});
 }
@@ -53,7 +62,7 @@ async function AddToDo(todoContent){
     
     fetch(url, options)
     .then(res => res.json())
-    .then((res) => { console.log(res); todoContainer.innerHTML = ''; GetToDos(); });
+    .then((res) => { console.log(res); GetToDos(); });
 }
 
 async function DeleteToDo(todoId){
@@ -63,14 +72,15 @@ async function DeleteToDo(todoId){
     
     fetch(url + '/'+ todoId, options)
     .then(res => res.json())
-    .then((res) => { console.log(res); todoContainer.innerHTML = ''; GetToDos(); });
+    .then((res) => { console.log(res); GetToDos(); });
 }
 
-async function UpdateToDo(element){
+async function UpdateToDo(element, todoContent){
     const childs = element.childNodes;
+    const contentCheck = todoContent === "" ? childs[1].innerHTML : todoContent;
     const todo = {
         isCompleted: childs[0].checked,
-        content: childs[1].innerHTML,
+        content: contentCheck,
         id: element.dataset.id
     }
 
@@ -84,8 +94,42 @@ async function UpdateToDo(element){
 
     fetch(url + '/' + element.dataset.id, options)
     .then(res => res.json())
-    .then((res) => { console.log(res); todoContainer.innerHTML = ''; GetToDos(); })
+    .then((res) => { console.log(res); GetToDos(); })
 }
 
+function EditMode(element){
+    const childs = element.childNodes;
+    
+    const editText = document.createElement("input");
+    editText.type = "text";
+    editText.value = childs[1].innerHTML;
+    element.replaceChild(editText, childs[1]);
+
+    const saveButton = document.createElement("button");
+    saveButton.type = "button";
+    saveButton.innerHTML = "Save";
+    saveButton.addEventListener("click", (element) => { SaveToDo(element.target.parentNode) })
+    element.replaceChild(saveButton, childs[2]);
+
+    const discardButton = document.createElement("button");
+    discardButton.innerHTML = "Discard";
+    discardButton.type = "button";
+    discardButton.addEventListener("click", () => { GetToDos() });
+    element.replaceChild(discardButton, childs[3]);
+
+    const editIcon = document.createElement("img");
+    editIcon.src = "src/icons/edit.png";
+    editIcon.classList.add("edit-icon");
+    const editIconContainer = document.createElement("div");
+    editIconContainer.classList.add("edit-icon-container");
+    editIconContainer.appendChild(editIcon);
+    element.replaceChild(editIconContainer, childs[0]);
+    
+    console.log(element);
+}
+
+function SaveToDo(element){
+    UpdateToDo(element, element.childNodes[1].value);
+}
 GetToDos();
 
